3f6fe6ea645238af437bdb2ab6c00e39facff149
ddddddO <lbfdeatq@gmail.com>
2023-04-29 20:30:14 +0900

    nits

--- Main.hs
+++ Main.hs
@@ -12,20 +12,17 @@ main = do
   print "   Fizz Buzz!   "
   print "----------------"
 
+  let rets = [ fizzBuzz nums
+             , fizzBuzz' nums
+             , fizzBuzz'' nums
+             , fizzBuzz''' nums
+             , fizzBuzz'''' nums
+             ]
+
   -- ["fizzbuzz","1","2","fizz","4","buzz","fizz","7","8","fizz","buzz","11","fizz","13","14","fizzbuzz","16","17","fizz","19","buzz","fizz","22","23","fizz","buzz","26","fizz","28","29","fizzbuzz","31","32","fizz","34","buzz","fizz","37","38","fizz","buzz","41","fizz","43","44","fizzbuzz","46","47","fizz","49","buzz"]
-  let ret1 = fizzBuzz nums
-      ret2 = fizzBuzz' nums
-      ret3 = fizzBuzz'' nums
-      ret4 = fizzBuzz''' nums
-      ret5 = fizzBuzz'''' nums
+  print $ head rets
 
-  print ret1
-  -- print ret2
-  -- print ret3
-  -- print ret4
-  -- print ret5
-
-  test [ret1, ret2, ret3, ret4, ret5]
+  test rets
 
 
 test :: [[String]] -> IO()
