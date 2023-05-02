02525c5885883ea9842657eafdfc15e0192fe390
ddddddO <lbfdeatq@gmail.com>
2023-05-02 23:14:02 +0900

    Binary Search Tree : http://learnyouahaskell.com/making-our-own-types-and-typeclasses#recursive-data-structures

--- Main.hs
+++ Main.hs
@@ -54,3 +54,33 @@ max4 = max 4
 
 composition = replicate 2 . product . map (*3) $ zipWith max [1, 2] [4, 5]
 testComp = composition == (replicate 2 (product (map (*3) (zipWith max [1, 2] [4, 5]))))
+
+-- Binary Search Tree : http://learnyouahaskell.com/making-our-own-types-and-typeclasses#recursive-data-structures
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a  = Node a (treeInsert x left) right
+  | x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x < a  = treeElem x left
+  | x > a  = treeElem x right
+
+-- ghci> :l Main.hs
+-- ghci> let nums = [8,6,4,1,7,3,5]
+-- ghci> let numsTree = foldr treeInsert EmptyTree nums
+-- ghci> numsTree
+-- Node 5 (Node 3 (Node 1 EmptyTree EmptyTree) (Node 4 EmptyTree EmptyTree)) (Node 7 (Node 6 EmptyTree EmptyTree) (Node 8 EmptyTree EmptyTree))
+-- ghci> 8 `treeElem` numsTree
+-- True
+-- ghci> 100 `treeElem` numsTree
+-- False
