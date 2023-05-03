4e539a4643ca101db6d34e054e45874849f26106
ddddddO <lbfdeatq@gmail.com>
2023-05-03 09:39:06 +0900

    print -> putStrLn

--- Main.hs
+++ Main.hs
@@ -10,9 +10,9 @@ main = do
   let max = 50
       nums = [0..max]
 
-  print "----------------"
-  print "   Fizz Buzz!   "
-  print "----------------"
+  putStrLn "----------------"
+  putStrLn "   Fizz Buzz!   "
+  putStrLn "----------------"
 
   let rets = [ fizzBuzz nums
              , fizzBuzz' nums
@@ -23,18 +23,18 @@ main = do
 
   -- ["fizzbuzz","1","2","fizz","4","buzz","fizz","7","8","fizz","buzz","11","fizz","13","14","fizzbuzz","16","17","fizz","19","buzz","fizz","22","23","fizz","buzz","26","fizz","28","29","fizzbuzz","31","32","fizz","34","buzz","fizz","37","38","fizz","buzz","41","fizz","43","44","fizzbuzz","46","47","fizz","49","buzz"]
   print $ head rets
-  print ""
+  putStrLn ""
 
   test rets
 
 
 test :: [[String]] -> IO()
 test rets = do
-  print "----------------"
-  print "      Test      "
-  print "----------------"
+  putStrLn "----------------"
+  putStrLn "      Test      "
+  putStrLn "----------------"
 
-  print (
+  putStrLn (
           let succeeded = (length passed) == (length rets - 1); in case succeeded of
                 True  -> "Ok!"
                 False -> "Fail..."
