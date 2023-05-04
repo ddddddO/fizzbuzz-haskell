e6a671965764a793597874fd21a7c5f659240f8e
ddddddO <lbfdeatq@gmail.com>
2023-05-04 22:04:14 +0900

    impl tree

--- _sandbox/command/Main.hs
+++ _sandbox/command/Main.hs
@@ -9,3 +9,26 @@ ls :: String -> IO [String]
 ls dir = do
   dirs <- getDirectoryContents dir
   return dirs
+
+tree :: String -> IO ()
+tree path = do
+  putStrLn path
+  paths <- ls path
+  let dirs = map (\p -> path ++ "/" ++ p) $ filter (\p -> not (p == path || p == "." || p == "..")) paths
+  mapM_ (\p -> do
+          existsDir <- doesDirectoryExist p
+          case existsDir of
+            True  -> tree p
+            False -> putStrLn p
+        ) dirs
+-- ghci> :l Main.hs
+-- ghci> tree "gtree"
+-- gtree
+-- gtree/tree.go
+-- gtree/testdata
+-- gtree/testdata/sample2.md
+-- gtree/testdata/sample1.md
+-- gtree/cmd
+-- gtree/cmd/gtree
+-- gtree/cmd/gtree/main.go
+-- gtree/makefile
